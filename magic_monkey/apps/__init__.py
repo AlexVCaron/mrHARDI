e774620f67c1a418cb1feadc393b5780442cf4e6
Alex Valcourt Caron <alex.valcourt.caron@usherbrooke.ca>
2020-08-23 20:11:41 -0400

    PFT tested. Implemented Life partially, still not certain, we'll let this one on the side for now.

--- magic_monkey/apps/__init__.py
+++ magic_monkey/apps/__init__.py
@@ -2,4 +2,5 @@ from .denoise import *
 from .measure import *
 from .reconstruct import *
 from .register import *
+from .track import *
 from .utils import ApplyMask, B0Utils, Concatenate
---
+++ magic_monkey/apps/track/__init__.py
@@ -0,0 +1,1 @@
+from .pft_tracking import PftTracking
