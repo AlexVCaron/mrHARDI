-> Minor fixes in bounding box fit -> Fixed changing datatypes of b0 apps -> Enforced datatypes when using get_fdata -> Added masking capabilities to antsRegistration app -> Lean down of heavy base singularity in progress

magic_monkey/apps/utils/dimensions.py
@@ -5,6 +5,7 @@ import nibabel as nib
 import numpy as np
 from dipy.segment.mask import crop
 from scilpy.utils.util import voxel_to_world, world_to_voxel
+from scipy.linalg import qr, lu
 from traitlets import Unicode, Any, Dict
 
 from magic_monkey.base.application import MagicMonkeyBaseApplication, \
@@ -47,7 +48,7 @@ def crop_nifti(img, wbbox):
     new_affine = np.copy(affine)
     new_affine[0:3, 3] = translation[0:3]
 
-    return nib.Nifti1Image(data_crop, new_affine)
+    return nib.Nifti1Image(data_crop.astype(img.get_data_dtype()), new_affine)
 
 
 class FitToBox(MagicMonkeyBaseApplication):
@@ -110,7 +111,7 @@ class FitToBox(MagicMonkeyBaseApplication):
             )[voxel_maxs > image.shape[:3]]
 
             data = np.pad(
-                image.get_fdata().astype(image.get_data_dtype()),
+                image.get_fdata(dtype=image.get_data_dtype()),
                 pads.T.astype(int), 'constant', constant_values=self.fill_value
             )
 
@@ -123,13 +124,15 @@ class FitToBox(MagicMonkeyBaseApplication):
 
 _fit_aliases = {
     "in": "FitBox.image",
+    "ref": "FitBox.reference",
     "out": "FitBox.output",
     "pbox": "FitBox.pkl_box"
 }
 
 
 class FitBox(MagicMonkeyBaseApplication):
-    image = required_file(description="Input image to fit to the box")
+    image = required_file(description="Input image to fit the box to")
+    reference = required_file(description="Reference image for the box")
     output = output_file_argument()
 
     pkl_box = Unicode(
@@ -138,20 +141,35 @@ class FitBox(MagicMonkeyBaseApplication):
 
     aliases = Dict(default_value=_fit_aliases)
 
+    def _change_world(self, coords, ref_affine, to_affine):
+        p, l, u = lu(ref_affine[:3, :3])
+        q, r = qr(l @ u)
+        t = ref_affine[:3, -1]
+
+        tmp = ((p @ np.diag(np.sign(np.diag(r)))) @ q).T @ (coords - t)
+
+        p, l, u = lu(to_affine[:3, :3])
+        q, r = qr(l @ u)
+        t = to_affine[:3, -1]
+
+        return p @ np.diag(np.sign(np.diag(r))) @ q @ tmp + t
+
     def execute(self):
         image = nib.load(self.image)
+        ref_affine = nib.load(self.reference).affine
 
         setattr(sys.modules['__main__'], 'WorldBoundingBox', WorldBoundingBox)
         with open(self.pkl_box, 'rb') as pklf:
             bbox = pickle.load(pklf)
 
-        voxel_mins = world_to_voxel(bbox.minimums, image.affine)
-        voxel_maxs = world_to_voxel(bbox.maximums, image.affine)
-
-        for i in range(3):
-            if voxel_mins[i] > voxel_maxs[i]:
-                voxel_mins[i] = image.shape[i] - voxel_mins[i]
-                voxel_maxs[i] = image.shape[i] - voxel_maxs[i]
+        voxel_mins = world_to_voxel(
+            self._change_world(bbox.minimums, ref_affine, image.affine),
+            image.affine
+        )
+        voxel_maxs = world_to_voxel(
+            self._change_world(bbox.maximums, ref_affine, image.affine),
+            image.affine
+        )
 
         ref_bbox = WorldBoundingBox(
             voxel_to_world(voxel_mins, image.affine),

magic_monkey/apps/utils/image.py
@@ -30,7 +30,7 @@ class ApplyMask(MagicMonkeyBaseApplication):
         0, help="Value used to fill the image outside the mask"
     ).tag(config=True)
     dtype = Enum(
-        [np.int, np.long, np.float, np.float64, None], None, help="Output type"
+        [np.int, np.long, np.float, np.float64, None], help="Output type"
     ).tag(config=True)
 
     output = output_file_argument()
@@ -39,14 +39,14 @@ class ApplyMask(MagicMonkeyBaseApplication):
 
     def execute(self):
         data = nib.load(self.image)
-        mask = nib.load(self.mask).get_fdata().astype(bool)
+        mask = nib.load(self.mask).get_fdata(dtype=bool)
 
         dtype = self.dtype
         if dtype is None:
-            dtype = data.header.get_data_dtype()
+            dtype = data.get_data_dtype()
 
         out_data = apply_mask_on_data(
-            data.get_fdata(), mask, self.fill_value, dtype
+            data.get_fdata(dtype=dtype), mask, self.fill_value, dtype
         )
 
         nib.save(nib.Nifti1Image(out_data, data.affine), self.output)
@@ -91,7 +91,7 @@ class Concatenate(MagicMonkeyBaseApplication):
         reference_affine = dwi_list[0].affine
 
         out_dwi, out_bvals, out_bvecs = concatenate_dwi(
-            [dwi.get_fdata() for dwi in dwi_list],
+            [dwi.get_fdata(dtype=dwi.get_data_dtype()) for dwi in dwi_list],
             bvals_list,
             bvecs_list
         )
@@ -176,15 +176,18 @@ class SplitImage(MagicMonkeyBaseApplication):
         )
 
     def _load_split(self, i):
-        return nib.load(
+        img = nib.load(
             "{}_ax{}_{}.nii.gz".format(self.prefix, self.axis, i)
-        ).get_fdata()
+        )
+        return img.get_fdata(dtype=img.get_data_dtype())
 
     def _img_to_split(self):
         img = nib.load(self.image)
         metadata = load_metadata(self.image)
         self._affine = img.affine
-        for i, sub in enumerate(np.moveaxis(img.get_fdata(), self.axis, 0)):
+        for i, sub in enumerate(
+            np.moveaxis(img.get_fdata(dtype=img.get_data_dtype()), self.axis, 0)
+        ):
             mt = metadata.copy()
             try:
                 mt.topup_indexes = [metadata.topup_indexes[i]]
@@ -231,7 +234,7 @@ class ConvertImage(MagicMonkeyBaseApplication):
 
     def execute(self):
         img = nib.load(self.image)
-        arr = img.get_fdata()
+        arr = img.get_fdata(dtype=img.get_data_dtype())
         arr[~np.isclose(arr, 0)] = 1.
         nib.save(
             nib.Nifti1Image(
@@ -271,7 +274,7 @@ class ReplicateImage(MagicMonkeyBaseApplication):
         img = nib.load(self.image)
         ref = nib.load(self.reference)
 
-        data = img.get_fdata()
+        data = img.get_fdata(dtype=img.get_data_dtype())
         if self.index is not None:
             data = data[..., self.index]
 