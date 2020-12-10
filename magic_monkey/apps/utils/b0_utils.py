d0ca32de5b26ca3f28a101e66491686636d7d9e1
Alex Valcourt Caron <alex.valcourt.caron@usherbrooke.ca>
2020-12-09 23:23:03 -0500

    Updated and fixed apps to fit multiple reps from nextflow pipeline

--- magic_monkey/apps/utils/b0_utils.py
+++ magic_monkey/apps/utils/b0_utils.py
@@ -115,7 +115,7 @@ class B0Utils(MagicMonkeyBaseApplication):
         bvecs = np.loadtxt(self.bvecs) if self.bvecs else None
 
         data, bvals, bvecs = squash_b0(
-            in_dwi.get_fdata().atype(in_dwi.get_data_dtype()), bvals, bvecs,
+            in_dwi.get_fdata().astype(in_dwi.get_data_dtype()), bvals, bvecs,
             self.configuration.get_mean_strategy_enum(),
             self.configuration.ceil_value,
             **kwargs
