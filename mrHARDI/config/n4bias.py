90fc5116ebef59a1cd5fdf5b0e7823fe0d85f34c
Alex Valcourt Caron <alex.valcourt.caron@usherbrooke.ca>
2023-08-19 14:35:25 -0400

    Bind N4 knot distance to iteration pyramid

--- mrHARDI/config/n4bias.py
+++ mrHARDI/config/n4bias.py
@@ -23,7 +23,7 @@ class N4BiasCorrectionConfiguration(mrHARDIConfigurable):
     ).tag(config=True)
     threshold = Float(0.01).tag(config=True)
     spline_order = BoundedInt(None, 2, 3, allow_none=True).tag(config=True)
-    knot_distance = Float(8.).tag(config=True, required=True)
+    knot_distance = Float(4.).tag(config=True, required=True)
     filter_width = Float(0.15).tag(config=True)
     noise = Float(0.01).tag(config=True)
     bins = Integer(200).tag(config=True)
@@ -46,9 +46,11 @@ class N4BiasCorrectionConfiguration(mrHARDIConfigurable):
             optionals.append('--rescale-intensities 1')
 
         if self.spline_order:
+            n_stages = len(self.iterations)
             optionals.append(
                 "--bspline-fitting [{},{}]".format(
-                    self.knot_distance * voxel_size, self.spline_order
+                    self.knot_distance * voxel_size * n_stages,
+                    self.spline_order
                 )
             )
 
